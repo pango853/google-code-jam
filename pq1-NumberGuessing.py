46263887845bee1fa5ce4843f55ecd168129512f
Pango Chan <Pango853@gmail.com>
2018-04-01 22:22:03 +0900

    init

---
+++ pq1-NumberGuessing.py
@@ -0,0 +1,7 @@
+# coding: utf-8 -*-
+
+#################################
+# Practice Session 2018
+# Question 1
+#   Number Guessing
+#################################
---
+++ pq2-SenateEvacuation.py
@@ -0,0 +1,4 @@
+#pq1-NumberGuessing.py
+#pq2-SenateEvacuation.py
+#pq3-Steed2Cruise Control.py
+#pq4-BathroomStalls.py
